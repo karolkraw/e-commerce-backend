2d934dc650b19803221a933fec3436656860392f
Karol <karolkrawczyk8650@gmail.com>
2023-08-04 22:02:02 +0200

    add ProductCategoryController

---
+++ src/main/java/com/example/ecommercebackend/controller/ProductCategoryController.java
@@ -0,0 +1,39 @@
+package com.example.ecommercebackend.controller;
+
+import com.example.ecommercebackend.entity.ProductCategory;
+import com.example.ecommercebackend.service.ProductCategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class ProductCategoryController {
+    private final ProductCategoryService categoryService;
+
+    @Autowired
+    public ProductCategoryController(ProductCategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public List<ProductCategory> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public ProductCategory getCategoryById(@PathVariable Long id) {
+        return categoryService.getCategoryById(id);
+    }
+
+    @PostMapping
+    public ProductCategory saveCategory(@RequestBody ProductCategory category) {
+        return categoryService.saveCategory(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+    }
+}
