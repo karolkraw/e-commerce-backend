68b8d5224311445712cb01e691212e2fa01cb48d
Karol <karolkrawczyk8650@gmail.com>
2023-08-04 22:02:46 +0200

    add ProductServiceImpl

---
+++ src/main/java/com/example/ecommercebackend/service/impl/ProductServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.ecommercebackend.service.impl;
+
+import com.example.ecommercebackend.entity.Product;
+import com.example.ecommercebackend.repository.ProductRepository;
+import com.example.ecommercebackend.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        return optionalProduct.orElse(null);
+    }
+
+    @Override
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
