bdc493d097c846336aeb7dd94289346510149c69
Karol <karolkrawczyk8650@gmail.com>
2023-08-05 15:05:56 +0200

    add ProductDto

---
+++ src/main/java/com/example/ecommercebackend/dto/ProductDto.java
@@ -0,0 +1,21 @@
+package com.example.ecommercebackend.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+public class ProductDto {
+    private Long id;
+    private Long categoryId;
+    private String sku;
+    private String name;
+    private String description;
+    private BigDecimal unitPrice;
+    private String imageUrl;
+    private boolean active;
+    private int unitsInStock;
+    private Date dateCreated;
+    private Date lastUpdated;
+}
