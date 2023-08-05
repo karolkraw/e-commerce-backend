9e38680d1697b1d15d342e3941e8a1de013d564b
Karol <karolkrawczyk8650@gmail.com>
2023-08-05 15:06:15 +0200

    refactor Product

--- src/main/java/com/example/ecommercebackend/entity/Product.java
+++ src/main/java/com/example/ecommercebackend/entity/Product.java
@@ -1,5 +1,6 @@
 package com.example.ecommercebackend.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
