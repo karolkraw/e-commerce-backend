ee61c5f386879ddb2325c10be37744ba130d9d85
Karol <karolkrawczyk8650@gmail.com>
2023-08-06 20:15:15 +0200

    refactor product service

--- src/main/java/com/example/ecommercebackend/Product/ProductService.java
+++ src/main/java/com/example/ecommercebackend/Product/ProductService.java
@@ -1,6 +1,5 @@
 package com.example.ecommercebackend.Product;
 
-import com.example.ecommercebackend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ProductService implements ProductService {
+public class ProductService {
     private final ProductRepository productRepository;
 
     @Autowired
@@ -16,23 +15,19 @@ public class ProductService implements ProductService {
         this.productRepository = productRepository;
     }
 
-    @Override
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
-    @Override
     public Product getProductById(Long id) {
         Optional<Product> optionalProduct = productRepository.findById(id);
         return optionalProduct.orElse(null);
     }
 
-    @Override
     public Product saveProduct(Product product) {
         return productRepository.save(product);
     }
 
-    @Override
     public void deleteProduct(Long id) {
         productRepository.deleteById(id);
     }
